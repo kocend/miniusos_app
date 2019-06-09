a832253922651048f591c53a568b5ee063eff354
kocend <48856972+kocend@users.noreply.github.com>
2019-06-09 17:40:03 +0200

    Initial commit

---
+++ src/main/java/com/miniusos_app/MiniusosAppApplication.java
@@ -0,0 +1,13 @@
+package com.miniusos_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniusosAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniusosAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/miniusos_app/controller/MainController.java
@@ -0,0 +1,31 @@
+package com.miniusos_app.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public ModelAndView getMain() {
+        ModelAndView m = new ModelAndView();
+        m.setViewName("/login.html");
+        return m;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView loggedIn(@RequestParam(value = "username",required = true) String username,
+                                 @RequestParam(value = "password",required = true) String password){
+        //@RequestParam(value = "option",required = true)String option,
+        ModelAndView m = new ModelAndView();
+        if("kocend".equals(username)&&"abc".equals(password))
+            m.setViewName("/hello");
+        else
+            m.setViewName("/login");
+        return m;
+    }
+}
