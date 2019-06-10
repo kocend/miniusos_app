1df1cf8afcb8cceb96a844cd6c31a89231bc17c9
kocend <48856972+kocend@users.noreply.github.com>
2019-06-10 14:25:06 +0200

    changes

---
+++ src/main/java/com/miniusos_app/service/PasswordEncode.java
@@ -0,0 +1,21 @@
+package com.miniusos_app.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncode {
+
+    BCryptPasswordEncoder passwordEncoder;
+
+    public PasswordEncode(){
+        passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public String encodePassword(String password){
+        return passwordEncoder.encode(password);
+    }
+
+    public static void main(String[] args) {
+        PasswordEncode pe = new PasswordEncode();
+    }
+
+}
