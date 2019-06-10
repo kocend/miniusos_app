925cf86709f54d47721f7b9fa0a28be170961f22
kocend <48856972+kocend@users.noreply.github.com>
2019-06-10 14:10:06 +0200

    new project

--- src/main/java/com/miniusos_app/controller/MainController.java
+++ src/main/java/com/miniusos_app/controller/MainController.java
@@ -1,52 +1,47 @@
 package com.miniusos_app.controller;
 
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 
-@Controller
+@RestController
 public class MainController {
-    @GetMapping("/")
-    public ModelAndView getMain() {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getHomePage() {
         ModelAndView m = new ModelAndView();
-        m.setViewName("login");
+        m.setViewName("hello");
         return m;
     }
 
-    @GetMapping("/student")
-    public ModelAndView getStudentData(){
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView getRegistrationForm(){
+        ModelAndView m = new ModelAndView();
+        m.setViewName("registration");
+        return m;
+    }
+
+    @RequestMapping(value = "/student", method = RequestMethod.GET)
+    public ModelAndView getStudentForm() {
         ModelAndView m = new ModelAndView();
         m.setViewName("student");
         return m;
     }
 
-    @GetMapping("/koordynator")
-    public ModelAndView getKoordynatorData(){
+    @RequestMapping(value = "/koordynator", method = RequestMethod.GET)
+    public ModelAndView getKoordynatorForm() {
         ModelAndView m = new ModelAndView();
         m.setViewName("koordynator");
         return m;
     }
 
-    @GetMapping("/pracownik_dziekanatu")
-    public ModelAndView getPracownikDziekanatuData(){
+    @RequestMapping(value = "/pracownik_dziekanatu", method = RequestMethod.GET)
+    public ModelAndView getPracownikDziekanatuForm() {
         ModelAndView m = new ModelAndView();
         m.setViewName("pracownik_dziekanatu");
         return m;
     }
 
-    @PostMapping("/login")
-    public ModelAndView loggedIn(@RequestParam(value = "username",required = true) String username,
-                                 @RequestParam(value = "password",required = true) String password){
-        //@RequestParam(value = "option",required = true)String option,
-        ModelAndView m = new ModelAndView();
-        if("kocend".equals(username)&&"abc".equals(password))
-            m.setViewName("hello");
-        else
-            m.setViewName("login");
-        return m;
-    }
 }
