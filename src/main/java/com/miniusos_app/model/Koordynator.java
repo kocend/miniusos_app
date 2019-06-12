66ca3813ec4000eff44a858bf7ef8f12227e74b5
bkoz494 <51702259+bkoz494@users.noreply.github.com>
2019-06-12 13:44:31 +0200

    Add files via upload
    koordynator id

--- src/main/java/com/miniusos_app/model/Koordynator.java
+++ src/main/java/com/miniusos_app/model/Koordynator.java
@@ -3,10 +3,12 @@ package com.miniusos_app.model;
 public class Koordynator {
     private String imie;
     private String nazwisko;
+    private Integer id;
 
-    public Koordynator(String imie,String nazwisko){
+    public Koordynator(String imie,String nazwisko, Integer id){
         this.imie=imie;
         this.nazwisko=nazwisko;
+        this.id=id;
     }
 
     public String getImie() {
@@ -23,5 +25,13 @@ public class Koordynator {
 
     public void setNazwisko(String nazwisko) {
         this.nazwisko = nazwisko;
+    }
+    
+    public Integer getId() {
+    	return id;
+    }
+    
+    public void setId(Integer id) {
+    	this.id=id;
     }
 }
